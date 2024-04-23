55058ae914d24cb5011c84ca26ef302ee0a2cbfc
dinith <c0b2302099@edu.teu.ac.jp>
2024-04-23 15:05:42 +0900

    練習問題２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import pygame as pg
-
+import random 
 
 WIDTH, HEIGHT = 1600, 900
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -17,10 +17,20 @@ DELTA = {
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #ここからこうかとんの設定
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    #ここから爆弾の設定
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
+    vx, vy = +5, +5
+
     clock = pg.time.Clock()
     tmr = 0
 
@@ -30,12 +40,14 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        # こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for k, v in DELTA.items():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
+            
         #if key_lst[pg.K_UP]:
         #    sum_mv[1] -= 5
         #if key_lst[pg.K_DOWN]:
@@ -46,6 +58,10 @@ def main():
         #    sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        # 爆弾の移動と表示
+        bd_rct.move_ip(vx, vy)
+        screen.blit(bd_img, bd_rct)
+        pg.display.update()
         
         pg.display.update()
         tmr += 1
