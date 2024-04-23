e415ff28bcb9ee48ed1133ac406cfae49389085e
dinith <c0b2302099@edu.teu.ac.jp>
2024-04-23 15:18:29 +0900

    練習問題３

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,20 @@ DELTA = {
 
 
 
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんRect，または，爆弾Rectの画面内外判定用の関数
+    引数：こうかとんRect，または，爆弾Rect
+    戻り値：横方向判定結果，縦方向判定結果（True：画面内／False：画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: 
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -29,11 +43,10 @@ def main():
     bd_rct = bd_img.get_rect()
     bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     vx, vy = +5, +5
-    vx, vy = +5, +5
+    #vx, vy = +5, +5
 
     clock = pg.time.Clock()
     tmr = 0
-
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -47,7 +60,10 @@ def main():
             if key_lst[k]:
                 sum_mv[0] += v[0]
                 sum_mv[1] += v[1]
-            
+        kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)
         #if key_lst[pg.K_UP]:
         #    sum_mv[1] -= 5
         #if key_lst[pg.K_DOWN]:
@@ -56,11 +72,15 @@ def main():
         #    sum_mv[0] -= 5
         #if key_lst[pg.K_RIGHT]:
         #    sum_mv[0] += 5
-        kk_rct.move_ip(sum_mv)
-        screen.blit(kk_img, kk_rct)
+
         # 爆弾の移動と表示
         bd_rct.move_ip(vx, vy)
         screen.blit(bd_img, bd_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:  # 横方向にはみ出てたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出てたら
+            vy *= -1
         pg.display.update()
         
         pg.display.update()
